2f762be3079cab7f2846c7428f083c13b072105d
Szymon Nowakowski <szymon.nowakowski@gmail.com>
2026-07-16 13:21:01 +0200

    making the problem dim smaller

--- clusterLearn_demo_exact.R
+++ clusterLearn_demo_exact.R
@@ -22,7 +22,7 @@ set.seed(rng)
 # 2. Simulation Parameters & Setup
 # ==============================================================================
 n_cat  <- 3L      # Number of categorical predictors
-levels <- 5L      # Number of levels of each categorical predictor
+levels <- 9L      # Number of levels of each categorical predictor
 
 # ==============================================================================
 # 3. Create true beta^*
